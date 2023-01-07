4e7ea44e315a45853437ac4add188284f265dbd9
Amy Yeung <amyyeung17@gmail.com>
2023-01-07 15:36:37 -0500

    Changed RouterLink back to a tag

--- src/components/projects/Card.vue
+++ src/components/projects/Card.vue
@@ -24,7 +24,7 @@ defineProps({
     </p>
     <p class="text-primary-100 mt-2"> <span class="bi bi-code-slash text-secondary mx-1"></span> {{ item.made }} </p>
     <div class="flex self-end text-primary-300">
-      <RouterLink :class="linkStyle + 'mx-2 underline-offset-4'" :to="item.live"> Live </RouterLink> /
+      <a :class="linkStyle + 'mx-2 underline-offset-4'" :href="item.live" target="_blank"> Live </a> /
       <a :class="linkStyle + 'mx-2 underline-offset-4'" href="" target="_blank"> Code </a> 
     </div>
   </div>
