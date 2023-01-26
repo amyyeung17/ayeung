8ea4ccc297828514dcc7cfd02abc3c4c39812a82
Amy Yeung <amyyeung17@gmail.com>
2023-01-25 22:07:16 -0500

    Updated text

--- src/components/about/Filler.vue
+++ src/components/about/Filler.vue
@@ -1,14 +1,11 @@
 <script setup>
 import { ref } from 'vue'
-</script>
 
-<template>
-  <div class="flex flex-col items-center px-3">
-    <h2 class="text-secondary text-xl font-medium"> Check out my final thesis </h2>
-    <p class="text-primary-100 font-medium text-center my-2"> 
-      A synthesis of artificial intelligence and its usage in healthcare today.
+/**
+ *  A synthesis of artificial intelligence and its usage in healthcare today.
       I briefly delve into the evolution of both the computer and artificial intelligence, 
       discuss some common natural languge processing (NLP) techniques, and examine their applications in dementia, mainly Alzheimer's, research.
+      
       Studies have shown early detection of Alzheimer's or Mild Cognitive Impariment (MCI) allow current treatments and preventative care to be more effective. 
       However, most cases are often left unexamined until much later stages when significant impairments have already occurred. Current exams are shown to be
       not as suited towards detecting early cases when administered. While biomarkers are typically accepted as one of the more definitive diagnosis, an examination of this kind is not
@@ -16,6 +13,16 @@ import { ref } from 'vue'
       thresholds used for diagnosis are debated. New research has emerged showing the potential of observing syntatic, semantic, and other features of an individual's speech.
       Those diagnosed with Alzheimer's have shown to have a decrease in syntatic complexity and lexical diversity. Furthermore, there have been studies that have shown this method's prospects
       of differentiating between dementia types. 
+ */
+</script>
+
+<template>
+  <div class="flex flex-col items-center px-3">
+    <h2 class="text-secondary text-xl font-medium"> Check out my final thesis </h2>
+    <p class="text-primary-100 font-medium text-center my-2"> 
+      A synthesis of artificial intelligence and its usage in healthcare.
+      I briefly delve into the evolution of both the computer and artificial intelligence, 
+      discuss common natural languge processing (NLP) techniques, and examine their applications in dementia, mainly Alzheimer's, research today.
     </p>
     <button class="bg-secondary text-primary rounded-full border-2 border-secondary my-2"> Show more info </button>
     <button class="bg-secondary text-primary rounded-full border-2 border-secondary my-2"> Download </button>
