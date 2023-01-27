da71d35d757431eef2f32d0a012b01acabeae9b9
Amy Yeung <amyyeung17@gmail.com>
2023-01-26 23:52:29 -0500

    Change: margins for Contact < md breakpoint

--- src/components/contact/Personal.vue
+++ src/components/contact/Personal.vue
@@ -4,13 +4,13 @@ import { linkStyle } from '../reusable/style.js'
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center h-3/4 w-full py-2 px-3"> 
+  <div class="flex flex-col items-center justify-center h-3/4 w-full py-2 md:px-3"> 
     <div 
       v-for="(i, index) of contactInfo"
-      class="flex items-center w-full my-1 px-3"
+      class="flex items-center w-full my-1 md:px-3"
       :key="index"
     > 
-      <span :class="`bi bi-${i.icon} text-primary-300 text-2xl my-2 mx-3`"> </span>
+      <span :class="`bi bi-${i.icon} text-primary-300 text-2xl my-2 md:ml-3 mr-3`"> </span>
         <a :class="linkStyle + ' text-xl mx-1 my-2 hover:text-secondary hover:underline underline-offset-4'" :href="i.link" :target="i.text !== 'Email' && '_blank'"> {{i.linktext}} </a>
     </div>
   </div>
