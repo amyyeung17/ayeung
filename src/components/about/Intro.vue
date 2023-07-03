c9c2e8d2db36b5d2d933801a64823eb61d75ee26
Amy Yeung <amyyeung17@gmail.com>
2023-07-03 15:06:04 -0400

    Updated introduction

--- src/components/about/Intro.vue
+++ src/components/about/Intro.vue
@@ -6,15 +6,18 @@ import { linkStyle } from '../reusable/style.js'
   <div class="text-primary-100 p-2 relative top-4 whitespace-pre-line">
     I'm a learner that believes every experience can be an opportunity to grow from.
     I strive to work towards my goals and improve as much as I can each day, whether it be work-related or personal.
-    I'm passionate about helping others accompish their own goals and have worked extensively in education, especially in STEM related fields. 
+    I'm passionate about helping others accompish their own goals and have worked extensively in STEM education. 
     {{`\n \n`}}
-    As a developer, I've been exploring more of the frontend side by working on
-    some side-projects. For each project, I attempt something novel to me, such as using a 
-    new framework. I find the learning process to be valuable not only for broadening my skill set,
-    but also for deepening my understanding overall. My most recent endeavor is learning Vue.
+    As a developer, I've been recently exploring more of the frontend side through
+    side-projects. With each project, I enjoy challenging myself by integrating
+    novel tools, skills, languages, etc. I find the learning process itself to be 
+    valuable for not only acquiring new knowledge, but also helping deepen my understanding of other materials. 
+    Nothing beats the light bulb going off in my head when everything falls into the right pieces.  
+    My most recent endeavor has been experimenting Next.js and GraphQL. 
     {{`\n \n`}}
-    In my downtime, I've been trying to improve my 5k time and to finish my huge backlog of games. 
-    I'm a big rowing enthuiast from my time spent as a coxswain in college. 
+    In my downtime, I've been running a lot and playing Genshin Impact & Honaki Star Rail. 
+    I recently went on a huge 2-week trip to Japan and South Korea with my friends. Needless to say,
+    I want to go back again already. :,)
     {{`\n \n`}}
     Talk to me anytime, including opinions on my wacky London fit, at: <a :class="linkStyle + ' underline-offset-4 inline'" href="mailto:amyyeung17@gmail.com">amyyeung17@gmail.com</a>.
   </div>
